cf667edd1bad5e97a395e480b72665158590e742
elkliueva <128966126+elkliueva@users.noreply.github.com>
2024-10-06 16:40:08 +0300

    Create task_11.py

---
+++ task_11.py
@@ -0,0 +1,29 @@
+class Dessert():
+
+    def __init__(self, name = None, calories = None):
+        self.name = name
+        self.calories = calories
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        self._name = value
+
+    @property
+    def calories(self):
+        return self._calories
+
+    @calories.setter
+    def calories(self, value):
+        self._calories = value
+
+    def is_healthy(self):
+        if self.calories < 200:
+            return True
+        return False
+    
+    def is_delicious(self):
+        return True

48f056ff92dd3df6b77f78294ba0f76b44242bc7
elkliueva <128966126+elkliueva@users.noreply.github.com>
2024-10-15 21:52:46 +0300

    Update task_12.py

--- task_12.py
+++ task_12.py
@@ -1,4 +1,5 @@
 class Dessert:
+
     def __init__(self, name=None, calories=None):
         self.name = name
         self.calories = calories
@@ -18,14 +19,16 @@ class Dessert:
     @calories.setter
     def calories(self, value):
         if value is not None and not isinstance(value, (int, float)):
-            raise ValueError("Calories must be a number or None")
-        self._calories = value
+            self._calories = None 
+        else:
+            self._calories = value  
 
     def is_healthy(self):
         return self.calories is not None and self.calories < 200
 
     def is_delicious(self):
         return True
+
 
 
 class JellyBean(Dessert):

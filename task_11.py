3d7a1d9be959bcc594b77b9e0f85f24cbb50565b
elkliueva <128966126+elkliueva@users.noreply.github.com>
2024-10-15 21:33:34 +0300

    Update task_11.py

--- task_11.py
+++ task_11.py
@@ -1,6 +1,6 @@
-class Dessert():
+class Dessert:
 
-    def __init__(self, name = None, calories = None):
+    def __init__(self, name=None, calories=None):
         self.name = name
         self.calories = calories
 
@@ -19,11 +19,12 @@ class Dessert():
     @calories.setter
     def calories(self, value):
         if value is not None and not isinstance(value, (int, float)):
-            raise ValueError("Calories must be a number or None")
-        self._calories = value
+            self._calories = None 
+        else:
+            self._calories = value  
 
     def is_healthy(self):
         return self.calories is not None and self.calories < 200
-    
+
     def is_delicious(self):
         return True
